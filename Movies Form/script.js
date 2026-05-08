a9d276b452d7241a2f00e5140aa6bbb6d8ee966c
idontknowwhattouseasauser <kh_pr@hotmail.com>
2026-05-08 20:10:59 +0100

    Add files via upload

---
+++ Movies Form/script.js
@@ -0,0 +1,32 @@
+class Movie{
+    constructor(movie_show,title,director,year,sequels){
+        this.movie_show=movie_show;
+        this.title=title;
+        this.director=director;
+        this.year=year;
+        this.sequels=sequels;
+    }
+}
+const default_movie=[
+    {
+        movie_show: "movie",
+        title: "The Breadwinner",
+        director: "Nora Twomey",
+        year: "2017",
+        sequels:"no",
+    }
+    {
+        movie_show: "show",
+        title: "Young Sheldon",
+        director: "Jon Favreau",
+        year: "2017",
+        sequels:"yes",
+    }
+    {
+        movie_show: "movie",
+        title: "The Breadwinner",
+        director: "Nora Twomey",
+        year: "2017",
+        sequels:"no",
+    }
+]
